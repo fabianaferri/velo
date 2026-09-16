e0c460f1503c63c69961e7451211a2d6b4924126
fabianaferri <fabianaferri@gmail.com>
2026-09-15 22:15:30 -0300

    titulo de cenario alterado

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Web App deve estar online', async ({ page }) => {
+test('Web app deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5173');
 
   // Expect a title "to contain" a substring.
